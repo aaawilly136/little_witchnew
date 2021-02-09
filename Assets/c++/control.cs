cef7d3d43a054d76f198edaa45794cf64353bb78
KazeYan <77669862+aaawilly136@users.noreply.github.com>
2021-02-09 21:30:01 +0800

    體力ui介面完成

--- Assets/c++/control.cs
+++ Assets/c++/control.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class control: MonoBehaviour
 {
     [Header("跳躍速度"), Range(0, 1000)]
     public float jump = 10;
-    [Header("移動速度"), Range(0, 1000)]
+    [Header("跑步移動速度"), Range(0, 1000)]
     public float speed = 10;
+    [Header("走路移動速度"), Range(0, 1000)]
+    public float speedwalk = 10;
     [Header("攝影旋轉速度"), Range(0, 1000)]
     public float turn = 10;
     [Header("攝影機角度限制")]
@@ -17,7 +20,31 @@ public class control: MonoBehaviour
     [Header("檢查地板球體位移")]
     public Vector3 offset;
     [Header("檢查地板球體位移")]
-    public int jumpCountLimit = 2;
+    public int jumpCountLimit = 1;
+    
+    //血條/魔力/體力
+    [Header("血量"), Range(0, 5000)]
+    public float hp = 100;
+    private float hpMax;
+    [Header("魔力"), Range(0, 5000)]
+    public float mp = 500;
+    private float mpMax;
+    [Header("體力"), Range(0, 5000)]
+    public float ps = 200;
+    private float psMax;
+
+    [Header("吧條")]
+    public Image barHp;
+    public Image barMp;
+    public Image barPs;
+
+    [Header("移動時每秒扣除體力"), Range(0, 5000)]
+    public float psMove = 15;
+    [Header("跳躍時扣除體力"), Range(0, 5000)]
+    public float psJump = 25;
+    [Header("休息時每秒恢復體力"), Range(0, 5000)]
+    public float psRecover = 100;
+
 
 
     private int jumpCount;
@@ -40,16 +67,22 @@ public class control: MonoBehaviour
         Ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody>();
         cam = GameObject.Find("攝影機根物件").transform;
+        hpMax = hp;
+        mpMax = mp;
+        psMax = ps;
 
     }
+    //呼叫系統
     private void Update()
     {
         Move();
         TurnCamera();
-    }
-    private void FixedUpdate()
-    {
         Jump();
+        PSSystem();
+    }
+    private void FixedUpdate() //50fps
+    {
+        
     }
     /// <summary>
     /// 移動方法
@@ -64,9 +97,20 @@ public class control: MonoBehaviour
 
         transform.rotation = Quaternion.Lerp(transform.rotation, camNew.rotation, 0.5f * turnspeed * Time.deltaTime); //角色的角度 = 角色與攝影機的差值
 
-        rig.velocity = ((camNew.forward * v + camNew.right * h) * speed * Time.deltaTime) + transform.up * rig.velocity.y;
+        if (ps>0)
+        {
+            rig.velocity = ((camNew.forward * v + camNew.right * h) * speed * Time.deltaTime) + transform.up * rig.velocity.y;
 
-        Ani.SetBool("跑步開", rig.velocity.magnitude > 0.1f);
+            Ani.SetBool("跑步開", rig.velocity.magnitude > 0.1f);
+        }
+        else
+        {
+            rig.velocity = ((camNew.forward * v + camNew.right * h) * speedwalk * Time.deltaTime) + transform.up * rig.velocity.y;
+
+            Ani.SetBool("走路開關", rig.velocity.magnitude > 0.1f);
+            Ani.SetBool("跑步開", false);
+        }
+        
     }
     private void TurnCamera()
     {
@@ -78,14 +122,6 @@ public class control: MonoBehaviour
     }
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpCountLimit)
-        {
-            jumpCount++;
-            rig.Sleep();
-            rig.WakeUp();
-            rig.AddForce(Vector3.up * jump);
-            Ani.SetTrigger("跳躍觸發");
-        }
         Collider[] hit = Physics.OverlapSphere(transform.position + offset, radius, 1 << 8);
         if (hit.Length > 0 && hit[0])
         {
@@ -95,5 +131,36 @@ public class control: MonoBehaviour
         else Isground = false;
 
         Ani.SetBool("跳躍是否在地上", Isground);
+
+        if (ps < psJump) return;
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpCountLimit - 1)
+        {
+            jumpCount++;
+            rig.Sleep();
+            rig.WakeUp();
+            rig.AddForce(Vector3.up * jump);
+            Ani.SetTrigger("跳躍觸發");
+            
+
+            ps -= psJump;
+            barPs.fillAmount = ps / psMax;
+
+        }
+        
+    }
+    //體力系統
+    private void PSSystem()
+    {
+        if (Ani.GetBool("跑步開"))
+        {
+            ps -= psMove * Time.deltaTime;
+            barPs.fillAmount = ps / psMax;
+        }
+        else if (!Ani.GetBool("走路開關"))
+        {
+            ps += psRecover * Time.deltaTime;
+            barPs.fillAmount = ps / psMax;
+        }
+        ps = Mathf.Clamp(ps, 0, psMax);
     }
 }
